c40020a89ce5cdb5e63cd0ec13eff1869af7fde2
SuperSalcedo22 <116348427+SuperSalcedo22@users.noreply.github.com>
2022-12-12 15:29:36 +0000

    Update streamlit_app.py
    putting the try and except in, removing kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,15 +27,19 @@ streamlit.dataframe(fruits_to_show)
 
 # new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("PLease select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    # normalises the json format
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    # outputs the normalised data into a table/dataframe
+    streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-# normalises the json format
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# outputs the normalised data into a table/dataframe
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 #troubleshooting
 streamlit.stop()
