8e4149b7b5a6c83105afed7677fbb528c8623cf5
SuperSalcedo22 <116348427+SuperSalcedo22@users.noreply.github.com>
2022-12-12 15:34:43 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 
 #creation of function to be called everytime
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
   # normalises the json format
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
